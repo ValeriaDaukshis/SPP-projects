db79b0f3b5dc802a11a0b62459eabb007ca3e19c
Valeryia Daukshys <valeryia_daukshys@epam.com>
2020-03-25 13:07:56 +0300

    clean front

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from 'app/main-page/main-page.component';
 import {TaskListComponent} from './tasks-manager/lists/task-list.component';
 import {TaskFormComponent} from './tasks-manager/forms/task-form.component';
 import {DetailTaskFormComponent} from './tasks-manager/forms/detailTask-form.component';
@@ -10,7 +9,6 @@ import { RegComponent } from './login-manager/forms/reg-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
-  { path: 'main', component: MainPageComponent },
   { path: 'tasks', component: TaskListComponent,  canActivate: [AuthGuard]},
   { path: 'task', component: TaskFormComponent },
   { path: 'task/:id', component: TaskFormComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
-import { MainPageComponent } from 'app/main-page/main-page.component';
 import { TaskService } from './tasks-manager/services/task.server';
 import {TaskListComponent} from './tasks-manager/lists/task-list.component';
 import {TaskFormComponent} from './tasks-manager/forms/task-form.component';
@@ -20,7 +19,6 @@ import { RegComponent } from './login-manager/forms/reg-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent,
     TaskListComponent,
     TaskFormComponent,
     DetailTaskFormComponent,
@@ -35,7 +33,6 @@ import { RegComponent } from './login-manager/forms/reg-form.component';
     AppRoutingModule,
   ],
   providers: [
-    // Angular providers
     HttpClient,
     TaskService,
     AuthenticationService,
